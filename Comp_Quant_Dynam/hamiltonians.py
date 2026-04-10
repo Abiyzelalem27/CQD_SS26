b18ac1f736a26911ed673aca228b100e6cf868cb
Abiyzelalem27 <abiyzelalem8@gmail.com>
2026-04-10 14:04:34 +0000

    A

--- Comp_Quant_Dynam/hamiltonians.py
+++ Comp_Quant_Dynam/hamiltonians.py
@@ -4,3 +4,5 @@ import numpy as np   # standard numerics
 sigmax = np.array([[0,1],[1,0]])
 sigmay = np.array([[0,-1j],[1j,0]])
 sigmaz = np.array([[1,0],[0,-1]])
+I = np.array([[1,0],[0,1]])
+ 
--- tests/utility_test.py
+++ tests/utility_test.py
@@ -1,17 +1,33 @@
 import numpy as np
-import Comp_Quant_Dynam.utility as util
+
+import numpy as np 
+import pytest
+from numpy.testing import assert_allclose
+
+from Comp_Quant_Dynam import (
+    I, X, Y, Z,
+)
 
 
-class Test_example_function:
+def test_I_gate():
+    """check that I is unitary"""
+    assert np.allclose(I.conj().T @ I, I)
 
-    def test_example_func_zero(self):
-        x = 0
-        expected =  1 / np.pi ** (1 / 4)
-        result = util.example_func(x)
-        assert np.allclose(expected, result)
 
-    def test_example_func_symmetry(self):
-        x = np.array([-1, 1])
-        result = util.example_func(x)
-        assert np.allclose(result[0], result[1])
+def test_sigmax():
+    """check that X is unitary and self-inverse"""
+    assert np.allclose(X.conj().T @ X, I)
+    assert np.allclose(X @ X, I)
+
+
+def test_sigmay():
+    """check that Y is unitary and self-inverse"""
+    assert np.allclose(Y.conj().T @ Y, I)
+    assert np.allclose(Y @ Y, I)
+
+def test_sigmaz():
+    """check that Z is unitary and self-inverse"""
+    assert np.allclose(Z.conj().T @ Z, I)
+    assert np.allclose(Z @ Z, I)  
+
 
